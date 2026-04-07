999381187baa2fb74a253d411193d453e222f68b
husnanhdt <husnandayat06@gmail.com>
2026-04-08 00:39:17 +0700

    menambahkan nama nim di views

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // Data mahasiswa (nanti bisa diganti dari database)
+    $mahasiswa = [
+        'nama' => 'Husnan Hidayat',
+        'nim' => '24.12.3217',
+        'kelas' => 'Digital Business 04',
+    ];
+
+    // Kirim data ke view 'welcome'
+    return view('welcome', compact('mahasiswa'));
 });
