4427a3783405f95534771daebfad6c91782cc706
husnanhdt <husnandayat06@gmail.com>
2026-04-10 07:58:04 +0700

    new code

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // Data mahasiswa (nanti bisa diganti dari database)
     $mahasiswa = [
         'nama' => 'Husnan Hidayat',
         'nim' => '24.12.3217',
         'kelas' => 'Digital Business 04',
     ];
 
-    // Kirim data ke view 'welcome'
     return view('welcome', compact('mahasiswa'));
+});
+
+Route::get('/tentang', function () {
+    return '<h1>Ini adalah halaman Tentang Aplikasi Event Hub</h1>';
+});
+
+Route::get('/kontak', function () {
+    return view('contact');
 });
