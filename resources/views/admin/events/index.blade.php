926b7f1ed351a2284a1e16ef200e7de8dc7a0ac8
husnanhdt <husnandayat06@gmail.com>
2026-05-24 20:11:58 +0700

    UTS_24.12.3217

--- resources/views/admin/events/index.blade.php
+++ resources/views/admin/events/index.blade.php
@@ -6,11 +6,7 @@
 
 @section('content')
 <!-- Header -->
-<header class="flex justify-between items-center mb-10">
-    <div>
-        <h1 class="text-3xl font-black">@yield('page_title')</h1>
-        <p class="text-slate-500 font-medium">@yield('page_subtitle')</p>
-    </div>
+<header class="flex justify-end items-center mb-10">
     <a href="{{ route('admin.events.create') }}" class="px-6 py-3 bg-indigo-600 text-white rounded-2xl font-bold shadow-lg shadow-indigo-100 hover:bg-indigo-700 active:scale-95 transition">
         + Tambah Event Baru
     </a>
